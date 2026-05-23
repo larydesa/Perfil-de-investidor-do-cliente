ce5a4d8f82c6a424ed8269976645bb688aa89a40
Larissa Sá <larissadasilvaaraujosa0@gmail.com>
2026-05-22 23:02:26 -0300

    pronto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Perfil de investidor do cliente
 Verificando se o perfil de um cliente é conservador, moderado ou arrojado em um investimento
+
+Nesse programa é criado uma carteira de investimento onde o cliente tem a opção de conhecer o perfil de investidor dele para aplicar o seu dinheiro da maneira correta, além disso, ele tem a opção de suporte ao cliente, seja por um agente virtual ou um especialista
---
+++ index.py
@@ -0,0 +1,21 @@
+print ("------- INVESTIMENTOS -------")
+valor_investido = float(input("Quanto você deseja investir: "))
+
+if valor_investido <= 5.000:
+  print("Seu perfil é: CONSERVADOR")
+elif valor_investido <= 50.000:
+  print("Seu perfil é: MODERADO")
+else:
+  print("Seu perfil perfil é: ARROJADO")
+print("-" * 30)
+atendimento = int(input('''Qual sua preferência de atendimento?
+                        [1] App/Robô
+                        [2] Especialista
+                        '''))
+if atendimento == 1:
+  print("Sua conta será direcionada para a nossa IA de investimentos recomendados")
+elif atendimento == 2:
+  if valor_investido <= 50.000:
+    print("Você terá acesso ao nosso Chat de suporte geral")
+  else:
+    print("Você terá um Gerente Exclusive dedicado para montar sua carteira de ações")
